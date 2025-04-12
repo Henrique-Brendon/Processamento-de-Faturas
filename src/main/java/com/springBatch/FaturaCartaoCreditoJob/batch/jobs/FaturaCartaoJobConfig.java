690b14681f598d3c9314c0871ad2f34c092e0978
henrique brendon <henriquebrendonti26@gmail.com>
2025-04-12 17:55:23 -0300

    Criar job faturaCartaoJob com suporte a JobRepository moderno

---
+++ src/main/java/com/springBatch/FaturaCartaoCreditoJob/batch/jobs/FaturaCartaoJobConfig.java
@@ -0,0 +1,29 @@
+package com.springBatch.FaturaCartaoCreditoJob.batch.jobs;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class FaturaCartaoJobConfig {
+
+    private final JobRepository jobRepository;
+
+    public FaturaCartaoJobConfig(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    @Bean
+    Job faturaCartaoJob(Step faturaCartaoCreditoStep) {
+        return new JobBuilder("faturaCartaoJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(faturaCartaoCreditoStep)
+                .build();
+    }
+}
